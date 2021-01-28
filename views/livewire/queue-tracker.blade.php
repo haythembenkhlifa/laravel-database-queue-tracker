f9d53e929755cd5ec37329ce430d6e11c8e7b93b
Haythem Ben Khlifa <benkhlifahaythem@gmail.com>
2021-01-28 14:17:38 +0200

    modal width update to fullscreen

--- views/livewire/queue-tracker.blade.php
+++ views/livewire/queue-tracker.blade.php
@@ -91,7 +91,7 @@
 
 <!-- Modal -->
 <div wire:ignore.self class="modal fade" id="errorModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-fullscreen-xxl-down" role="document">
+    <div class="modal-dialog modal-fullscreen" role="document">
        <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="errorModalTitle"></h5>
